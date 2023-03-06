4f5b323aad2fd5cfe813a9c7f6cae7b7d17e6f7b
max blanq <maxm_99@mail.ru>
2023-03-06 23:08:46 +0300

    исправил чат

--- Payroll.Web/Hub/ChatHub.cs
+++ Payroll.Web/Hub/ChatHub.cs
@@ -1,26 +1,30 @@
-﻿
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SignalR;
 using Payroll.Business.Services;
+using Payroll.DataAccess.Interfaces;
 using Payroll.DataAccess.Models;
+using Payroll.DataAccess.Models.Employees;
 
 namespace Payroll.Web.Hub
 {
     public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
     {
         private readonly ChatService _chatService;
+        private readonly EmployeeService _employeeService;
 
-        public ChatHub(ChatService chatService)
+        public ChatHub(ChatService chatService, EmployeeService employeeService)
         {
             _chatService = chatService;
+            _employeeService = employeeService;
         }
         public async Task SendMessage(string user, string message)
-        {
+        {   
             ChatMessage chatMessage = new ChatMessage(user, message);
             await _chatService.SaveMessage(chatMessage);
 
             await Clients.All.SendAsync("ReceiveMessage",
-                chatMessage.UserName, 
-                chatMessage.Message, 
+                chatMessage.UserName,
+                chatMessage.Message,
                 chatMessage.SendTime.ToString("HH:mm"));
         }
         public override async Task OnConnectedAsync()
@@ -29,8 +33,8 @@ namespace Payroll.Web.Hub
 
             foreach (var message in chatHistory)
             {
-                await Clients.Caller.SendAsync("ReceiveMessage", 
-                    message.UserName, 
+                await Clients.Caller.SendAsync("ReceiveMessage",
+                    message.UserName,
                     message.Message,
                     message.SendTime.ToString("HH:mm"));
             }
